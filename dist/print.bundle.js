ac7a20a5ae404626f0e0e4616f237831bf49537e
Aimable BANCUNGUYE <bancunguye66@gmail.com>
2023-03-29 17:34:13 +0300

    fix webpack

--- dist/print.bundle.js
+++ dist/print.bundle.js
@@ -1,18 +1,1 @@
-(self["webpackChunkwebpack_demo"] = self["webpackChunkwebpack_demo"] || []).push([["print"],{
-
-/***/ "./src/print.js":
-/*!**********************!*\
-  !*** ./src/print.js ***!
-  \**********************/
-/***/ (() => {
-
-
-
-/***/ })
-
-},
-/******/ __webpack_require__ => { // webpackRuntimeModules
-/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-/******/ var __webpack_exports__ = (__webpack_exec__("./src/print.js"));
-/******/ }
-]);
+(self.webpackChunkwebpack_demo=self.webpackChunkwebpack_demo||[]).push([[740],{845:()=>{}},e=>{e(e.s=845)}]);
