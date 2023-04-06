945dea6da76906518ca9058b2baa31989c5885d0
Aimable BANCUNGUYE <bancunguye66@gmail.com>
2023-04-06 18:03:00 +0300

    add jest test

--- src/__mock__/addList.js
+++ src/__mock__/addList.js
@@ -4,18 +4,11 @@ import display from './display.js';
 const addList = () => {
   const taskList = [];
 
-  taskList.push(
-    {
-      description: 'Complete my activities on time',
-      completed: false,
-      index: 0,
-    },
-    {
-      description: 'Completed my project',
-      completed: true,
-      index: 1,
-    }
-  );
+  taskList.push({
+    description: 'Complete my activities on time',
+    completed: false,
+    index: 0,
+  });
   localStorage.setItem('localItem', JSON.stringify(taskList));
 
   display();
---
+++ src/__test__/editcompleted.test.js
@@ -0,0 +1,45 @@
+import addList from '../__mock__/addList.js';
+import update from '../__mock__/updateList.js';
+import completedList from '../__mock__/completedList.js';
+import clear from '../__mock__/clearCompleted.js';
+
+describe('Test for update task lists', () => {
+  it('Check if list is updated', () => {
+    localStorage.removeItem('localItem');
+    addList('task 1');
+    update(0);
+    const after = JSON.parse(localStorage.getItem('localItem'));
+
+    expect(after).toEqual([
+      {
+        description: "I will complete my today's task",
+        completed: false,
+        index: 0,
+      },
+    ]);
+  });
+  it('Check if completed list is true', () => {
+    localStorage.removeItem('localItem');
+    addList('task 1');
+    completedList(0);
+    const after = JSON.parse(localStorage.getItem('localItem'));
+    expect(after).toEqual([
+      {
+        description: 'Complete my activities on time',
+        completed: true,
+        index: 0,
+      },
+    ]);
+  });
+});
+describe('Test for completed list task', () => {
+  it('Check completed list task is removed or not', () => {
+    localStorage.removeItem('localItem');
+    addList('task 1');
+    completedList(0);
+    const before = JSON.parse(localStorage.getItem('localItem'));
+    clear();
+    const after = JSON.parse(localStorage.getItem('localItem'));
+    expect(after.length).toBe(before.length - 1);
+  });
+});
