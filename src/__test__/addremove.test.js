8616735488eddba88b96fe5787f644d68a5d660d
rosemutai <chepngetichrose2030@gmail.com>
2023-04-06 16:13:03 +0300

    Added add-remove tests

--- src/__test__/addremove.test.js
+++ src/__test__/addremove.test.js
@@ -1,7 +1,7 @@
 import addList from '../__mock__/addList.js';
 import remove from '../__mock__/removeList.js';
 
-describe('Test for adding tasks', () => {
+describe('Test for adding a task to the tasks array', () => {
   it('Check array of object is not null', () => {
     localStorage.removeItem('localItem');
     addList('task 1');
@@ -9,7 +9,7 @@ describe('Test for adding tasks', () => {
     const SIZE = task.length;
     expect(SIZE).not.toBeNull();
   });
-  it('Added to the DOM', () => {
+  it('Task is added to the DOM', () => {
     localStorage.removeItem('localItem');
     addList('task 2');
     const task = JSON.parse(localStorage.getItem('localItem'));
@@ -17,7 +17,7 @@ describe('Test for adding tasks', () => {
   });
 });
 
-describe('Test for removing list', () => {
+describe('Test for removing a task from the list', () => {
   it('Checks list if remove todo size reduces or not', () => {
     localStorage.removeItem('localItem');
     addList('task 4');
@@ -28,7 +28,7 @@ describe('Test for removing list', () => {
     const newSIZE = newTasks.length;
     expect(newSIZE).toBe(SIZE - 1);
   });
-  it('Check is localStorage updated after remove', () => {
+  it('Check is localStorage updated after a todo is removed', () => {
     localStorage.removeItem('localItem');
     addList('task 5');
     const before = JSON.parse(localStorage.getItem('localItem'));
